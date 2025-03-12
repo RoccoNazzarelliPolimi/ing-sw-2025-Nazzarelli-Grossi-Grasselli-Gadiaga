ceb7a7ccaa9d23bac113395e22a62663b7d93f34
RoccoNazzarelliPolimi <rocco.nazzarelli@mail.polimi.it>
2025-03-12 16:13:42 +0100

    Classes

--- Model/src/main/java/Deck.java
+++ Model/src/main/java/Deck.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
-        private int startDeck1;
-        private int startDeck2;
-        private int startDeck3;
+        private Card [] Deck1;
+        private Card [] Deck2;
+        private Card [] Deck3;
+        private Card [] mainDeck;
+        private List<Card>  gameDeck;
+
+        public Deck (){
+                this.mainDeck = new Card[40];
+                this.Deck1 = new Card[3];
+                this.Deck2 = new Card[3];
+                this.Deck3 = new Card[3];
+        }
+
+        public setDeck()
+        {
+
+        }
+
+
+        public Card drawCard()
+        {
+                Card cardDrawn = new Card();
+
+                cardDrawn = this.gameDeck.get(0);
+                this.gameDeck.remove(0);
+
+                return cardDrawn;
+        }
+
+        public void removeCard()
+        {
+
+        }
+}
