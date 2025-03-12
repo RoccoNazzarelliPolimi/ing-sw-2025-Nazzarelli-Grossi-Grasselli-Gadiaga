604201f2d2ae5ccc0f4fb459f6221ee35b9a6846
David <david.02.gadiaga@gmail.com>
2025-03-12 12:12:37 +0100

    tentativo di commit

--- Model/src/main/java/Card.java
+++ Model/src/main/java/Card.java
@@ -1,2 +1,15 @@
 public class Card {
+    private String suit;
+    private int value;
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+
+    }
+    public String getSuit() {
+        return suit;
+    }
+    public int getValue() {
+        return value;
+    }
 }
