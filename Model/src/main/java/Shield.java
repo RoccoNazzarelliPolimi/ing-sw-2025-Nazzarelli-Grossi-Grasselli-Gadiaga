7c279e89798c491e43abb0cc3cd126027a01568a
anitagrossi <anitaangela.grossi@mail.polimi.it>
2025-03-13 09:42:06 +0100

    aggiunto shield

---
+++ Model/src/main/java/Shield.java
@@ -0,0 +1,25 @@
+public class Shield extends Tile {
+    private int[] sides;
+    private int[] defence; // 1 = scudo attivo su quel lato, 0 = nessuno scudo
+
+    // Costruttore
+    public Shield(int[] sides, int[] defence) {
+        this.sides = sides;
+        this.defence = defence;
+    }
+
+    // Metodo per ottenere i lati protetti dallo scudo
+    public int[] getDefence() {
+        return defence;
+    }
+
+    // Restituisce il tipo della Tile
+    public String checkTypeTile() {
+        return "Shield";
+    }
+
+    public int[] getSides() {
+        return sides;
+    }
+}
+
