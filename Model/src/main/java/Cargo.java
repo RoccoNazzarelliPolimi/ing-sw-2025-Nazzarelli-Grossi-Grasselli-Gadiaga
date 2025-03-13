c9434e139aff361c413688b559844bb238222f01
NicolasGrasselli <nicolas.grasselli@mail.polimi.it>
2025-03-13 14:54:36 +0100

    Storage creata, addCargo needs to be done better

---
+++ Model/src/main/java/Cargo.java
@@ -0,0 +1,21 @@
+
+    public class Cargo {
+        private Storage storage; // Magazzino in cui si trova il cargo
+        private int value;       // Valore del carico
+
+        // Costruttore
+        public Cargo(Storage storage, int value) {
+            this.storage = storage;
+            this.value = value;
+        }
+
+        // Getter per il valore del cargo
+        public int getValue() {
+            return value;
+        }
+
+    }
+
+
+
+
