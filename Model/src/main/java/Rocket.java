fd62085ae90f026f1c2dac7025d0579120c34589
NicolasGrasselli <nicolas.grasselli@mail.polimi.it>
2025-03-13 16:21:55 +0100

    Fixed error

--- Model/src/main/java/Rocket.java
+++ Model/src/main/java/Rocket.java
@@ -5,9 +5,10 @@ public class Rocket {
     private int order;
     private int numRounds;
     private int boardLocation;
+    private int rocketId;
 
-    public Rocket(int color, Player rocketPlayer, int boardLocation, int order, int numRounds)
-    {
+    // Costruttore
+    public Rocket(int color, Player rocketPlayer, int boardLocation, int order, int numRounds) {
         this.color = color;
         this.boardLocation = boardLocation;
         this.rocketPlayer = rocketPlayer;
@@ -15,6 +16,58 @@ public class Rocket {
         this.order = order;
     }
 
+    // Getter e Setter per color
+    public int getColor() {
+        return color;
+    }
 
+    public void setColor(int color) {
+        this.color = color;
+    }
 
+    // Getter e Setter per rocketPlayer
+    public Player getRocketPlayer() {
+        return rocketPlayer;
+    }
+
+    public void setRocketPlayer(Player rocketPlayer) {
+        this.rocketPlayer = rocketPlayer;
+    }
+
+    // Getter e Setter per order
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    // Getter e Setter per numRounds
+    public int getNumRounds() {
+        return numRounds;
+    }
+
+    public void setNumRounds(int numRounds) {
+        this.numRounds = numRounds;
+    }
+
+    // Getter e Setter per boardLocation
+    public int getBoardLocation() {
+        return boardLocation;
+    }
+
+    public void setBoardLocation(int boardLocation) {
+        this.boardLocation = boardLocation;
+    }
+
+    // Getter e Setter per rocketId
+    public int getRocketId() {
+        return rocketId;
+    }
+
+    public void setRocketId(int rocketId) {
+        this.rocketId = rocketId;
+    }
 }
+
